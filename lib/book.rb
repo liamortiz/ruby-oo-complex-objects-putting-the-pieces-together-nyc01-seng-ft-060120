25d3e4a1008fe8e410b6cacb9adefd0d53493dc4
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-03 13:12:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,6 +10,16 @@ class Book
   def author=(author)
     @author = author
   end
+  def author
+    @author
+  end
+  
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  def page_count
+    @page_count
+  end
 end
 
 book = Book.new("The Last Kingdom")
