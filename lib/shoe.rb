ca138903b7ad53f2fea3d57cdf0ba6c8c868f598
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-03 13:15:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+def Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end
+  def size=(size)
+    @size = size
